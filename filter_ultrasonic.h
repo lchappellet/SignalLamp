a72473e332ea1104d6a321088875e84772dd3cf5
Andrew Hoffman <andyhoffy@gmail.com>
2017-01-23 22:42:56 -0800

    Exponential moving average filter.

--- filter_ultrasonic.h
+++ filter_ultrasonic.h
@@ -4,14 +4,10 @@
 
 class Filter_ultrasonic
 {
-
      public:
       double average_ping_filter(double sonar_ping);
      private:
       double ping_value = 0.0;
-
-
-
 };
 
 #endif // FILTER_ULTRASONIC_H
